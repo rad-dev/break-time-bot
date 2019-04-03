03ed6286a98dec1eb74cd43948e99ac4ac89f5ec
Dagmang Cornellius <cornellius.dagmang@nokia.com>
2019-04-03 20:55:18 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,12 @@
+from slackbot.bot import Bot
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+
+
+def main():
+    bot = Bot()
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
